995fab2da38e7f8eed649df0513265a54e33f48c
Limnyn <99390399+limnyn@users.noreply.github.com>
2025-08-11 07:33:54 +0900

    [Gold II] Title: 연료 채우기, Time: 332 ms, Memory: 22404 KB -BaekjoonHub

--- 백준/Gold/1826. 연료 채우기/연료 채우기.java
+++ 백준/Gold/1826. 연료 채우기/연료 채우기.java
@@ -1,59 +1,59 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+
+/**
+ * https://www.acmicpc.net/problem/1826
+ *
+ * 1. 현재 가스로 다음 정류장 까지 갈 수 없으\면, 최대 힙에서 poll 한다 (count++)
+ *  -> 이전 정류장 중 가스를 가장 많이 주던 정류소를 들른것으로 친다.
+ * 2. 가스를 최대 힙에 넣는다.
+ * */
+
 
 public class Main {
-    //https://www.acmicpc.net/problem/1826
-
-    public static void main(String[] args) throws IOException{
+    
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int [][] gasStations = new int[n+1][2];
-    
-        for(int i = 0; i < n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int loc = Integer.parseInt(st.nextToken());
-            int gas = Integer.parseInt(st.nextToken());
-            gasStations[i][0] = loc;
-            gasStations[i][1] = gas;
+        StringTokenizer st;
 
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[][] gasStation = new int[N + 1][2]; //주유소 위치, 연료 양
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            gasStation[i][0] = Integer.parseInt(st.nextToken());
+            gasStation[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int end = Integer.parseInt(st.nextToken());
-        int left_gas = Integer.parseInt(st.nextToken());
-        gasStations[n][0] = end;
-        Arrays.sort(gasStations, (o1, o2) -> {
-            return o1[0]-o2[0];            
-        });
+        // 마지막 줄 입력
+        st = new StringTokenizer(br.readLine());
+        gasStation[N][0] = Integer.parseInt(st.nextToken()); // 마지막 목적지도 지나가는 경로에 포함
+        int remainGas = Integer.parseInt(st.nextToken());
 
+        // 입력이 주유소 가까운 순 정렬이 아닐수 도 있기 때문에 정렬 처리
+        Arrays.sort(gasStation, Comparator.comparingInt(o -> o[0]));
 
-        PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
-        int gas_visit_count = 0;
-        for (int i = 0; i <= n; i++){
-            
-            if (left_gas < gasStations[i][0]){
-                
-                while (left_gas < gasStations[i][0]) {
-                    if (pQ.peek() == null){
-                        System.out.println(-1);
-                        return;
-                    }
-                    left_gas += pQ.poll();
-                    gas_visit_count += 1;
+        int count = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i = 0; i < N + 1; i++) {
+            int gasStationDistance = gasStation[i][0];
+            int gas = gasStation[i][1];
+
+            while (remainGas < gasStationDistance) {
+                if (pq.isEmpty()){
+                    System.out.println(-1);
+                    return;
                 }
+                remainGas += pq.poll();
+                count += 1;
             }
-            pQ.add(gasStations[i][1]);
+            pq.offer(gas);
         }
-        System.out.println(gas_visit_count);
-        
+
+        System.out.println(count);
     }
-    
-    
 }
