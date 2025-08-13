90d6510eb43b3a95cc698ea81e3ec5b87185c2d0
Limnyn <99390399+limnyn@users.noreply.github.com>
2025-08-13 11:35:54 +0900

    [Gold IV] Title: N-Queen, Time: 11244 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,64 +1,24 @@
-# https://www.acmicpc.net/problem/9663
-"""
-N-Queen
-dfs로 조합을 만들며 불가능 한 경우 가지치기
-
-놓을 수 있는 경우
-1. 같은 열에 이미 퀸이 있으면 불가능
-2. 같은 대각선에 이미 퀸이 있으면 불가능
-그 외에 해당 행 열에 놓을 수 있음
-
-대각 "/" == r+c
-/ 1 2 3 4
-1 2 3 4 5
-2 3 4 5 6
-3 4 5 6 7
-4 5 6 7 8
-
-(r, c)에 대해서 / 방향 대각선은 r+c가 같은 값들이다, 
-즉 /방향 대각선은 (2n+1) boolean 배열로 값을 파악할 수 있다.
-
-대각 "\" == r-c
-\ 1 2 3 4
-1 0 -1 -2 -3
-2 1 0 -1 -2
-3 2 1 0 -1
-4 3 2 1 0
-
--(n-1)부터 n-1까지 값으로 처리 가능
-이를 n을 더해서
-1~2n-1 값으로 처리 가능
-"""
-
 import sys
-def input(): return sys.stdin.readline().rstrip()
+input = sys.stdin.readline
 
+n = int(input())
+FULL = (1 << n) - 1   # n개의 열 비트가 1
+ans = 0
 
-
-def dfs(r):
-    global count
-    
-    if r == n:
-        count += 1
+def dfs(row: int, cols: int, d1: int, d2: int):
+    # cols: 사용중인 열, d1: \ 대각(왼쪽으로 쉬프트), d2: / 대각(오른쪽으로 쉬프트)
+    global ans
+    if row == n:
+        ans += 1
         return
-    
-    for c in range(n):
-        d1 = r + c
-        d2 = r - c + n
-        
-        if not diags1[d1] and not diags2[d2] and not col[c]:
-            col[c] = diags1[d1] = diags2[d2] = True
-            dfs(r + 1)
-            col[c] = diags1[d1] = diags2[d2] = False
-            
-        
+    avail = FULL & ~(cols | d1 | d2)   # 이번 행에서 놓을 수 있는 자리들
+    while avail:
+        bit = avail & -avail           # 최하위 1비트 뽑기
+        avail -= bit
+        dfs(row + 1,
+            cols | bit,
+            (d1 | bit) << 1 & FULL,
+            (d2 | bit) >> 1)
 
-if __name__== "__main__":
-    n = int(input())
-    count = 0
-    col = [False] * n
-    diags1 = [False] * (2*n + 1) # /
-    diags2 = [False] * (2*n + 1) # \
-    
-    dfs(0)
-    print(count)
+dfs(0, 0, 0, 0)
+print(ans)
